acccc3c153d748622cf0d8766ddc315ef2755d41
Sonya Korolev Shapiro <sonyakorolevshapiro@Sonyas-MacBook-Air.local>
2020-09-21 12:19:03 -0400

    add

--- typewriter.js
+++ typewriter.js
@@ -1,4 +1,8 @@
 const sentence = "hello there from lighthouse labs";
 for (const char of sentence) {
-    console.log(char);
+  setTimeout(() => {
+    process.stdout.write(char);
+  }, 1000) // <= 1s delay to make it noticeable. Can dial it down later.
+  
   }
+  
